d2dc35ad9b995f182ab026dadc0409e2825ca6b2
Shunsuke Suzuki <g1530124@gmail.com>
2020-09-21 13:28:20 +0900

    session ne　見れた

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class SessionsController < ApplicationController
+      def new
+        render json: { message: 'ok' }
+      end
+
+      def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user&.authenticate(params[:session][:password])
+          log_in user
+          render json: { message: 'ok' }
+        else
+          render json: { message: 'ng' }
+        end
+      end
+
+      def destroy
+        log_out if logged_in?
+        render json: { message: 'ok' }
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  namespace :api do
+    namespace :v1 do
+      get    '/login',   to: 'sessions#new'
+      post   '/login',   to: 'sessions#create'
+      delete '/logout',  to: 'sessions#destroy'
+    end
+  end
 end
