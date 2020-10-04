255493f2cbb756bf0e570dfe7e40d8d1103912e4
Shunsuke Suzuki <g1530124@gmail.com>
2020-10-04 19:02:16 +0900

    routing 修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      get    '/login',   to: 'sessions#new'
       post   '/login',   to: 'sessions#create'
       delete '/logout',  to: 'sessions#destroy'
 
